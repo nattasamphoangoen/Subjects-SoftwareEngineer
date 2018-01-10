724454da0121a4d49cf13e0f5f5e2fd684ab7aaa
natta <natta151515@hotmail.com>
2018-01-10 10:28:45 +0700

    app

--- src/main/js/app_old_syntax.js
+++ src/main/js/app_old_syntax.js
@@ -4,33 +4,129 @@ var ons = require('onsenui');
 var Ons = require('react-onsenui');
 var client = require('./client');
 
-var App = React.createClass({
+var MyPage = React.createClass({
+  getInitialState: function() {
+    return {
+      username: '',
+      password: '',
+      vegetables: [
+        'Tomato',
+        'Cucumber',
+        'Onion',
+        'Eggplant',
+        'Cabbage'
+      ],
+      selectedVegetable: 'Onion'
+    };
+  },
 
-	renderToolbar: function() {
+  renderToolbar() {
+    return (
+      <Ons.Toolbar>
+        <div className='center'>Form input</div>
+      </Ons.Toolbar>
+    );
+  },
 
-		return (
-			<Ons.Toolbar>
-				<div className='center'>Onsen UI</div>
-				<div className='right'>
-            		<Ons.ToolbarButton>
-              			<Ons.Icon icon='ion-navicon, material:md-menu'></Ons.Icon>
-            		</Ons.ToolbarButton>
-            	</div>
-			</Ons.Toolbar>
-		)
+  handleClick: function() {
+    if (this.state.username === 'bob' && this.state.password === 'secret') {
+      ons.notification.alert('You are now signed in!');
+    }
+    else {
+      ons.notification.alert('Username or password incorrect!');
+    }
+  },
 
-	},
+  handleUsernameChange(e) {
+    this.setState({username: e.target.value});
+  },
 
-	render: function() {
-		return(
-			<Ons.Page
-				renderToolbar={this.renderToolbar} >
-				<p>Hello World</p>
-			</Ons.Page>	
-		)
-	}
+  handlePasswordChange(e) {
+    this.setState({password: e.target.value});
+  },
 
+  handleVegetableChange(vegetable) {
+    this.setState({selectedVegetable: vegetable});
+  },
 
+  renderCheckboxRow(row) {
+    return (
+      <Ons.ListItem key={row} tappable>
+        <label className='left'>
+          <Ons.Checkbox
+            inputId={`checkbox-${row}`}
+          />
+        </label>
+        <label htmlFor={`checkbox-${row}`} className='center'>
+          {row}
+        </label>
+      </Ons.ListItem>
+    )
+  },
+
+  renderRadioRow(row) {
+    return (
+      <Ons.ListItem key={row} tappable>
+        <label className='left'>
+          <Ons.Radio
+            inputId={`radio-${row}`}
+            checked={row === this.state.selectedVegetable}
+            onChange={this.handleVegetableChange.bind(this, row)}
+          />
+        </label>
+        <label htmlFor={`radio-${row}`} className='center'>
+          {row}
+        </label>
+      </Ons.ListItem>
+    )
+  },
+
+  render: function() {
+    return (
+      <Ons.Page renderToolbar={this.renderToolbar}>
+        <section style={{textAlign: 'center'}}>
+          <p>
+            <Ons.SearchInput
+              placeholder='Search' />
+          </p>
+          <p>
+            <Ons.Input
+              value={this.state.username}
+              onChange={this.handleUsernameChange}
+              modifier='underbar'
+              float
+              placeholder='Username' />
+          </p>
+          <p>
+            <Ons.Input
+              value={this.state.password}
+              onChange={this.handlePasswordChange}
+              modifier='underbar'
+              type='password'
+              float
+              placeholder='Password' />
+          </p>
+          <p>
+            <Ons.Button onClick={this.handleClick}>Sign in</Ons.Button>
+          </p>
+        </section>
+
+        <Ons.List
+          dataSource={this.state.vegetables}
+          renderHeader={() => <Ons.ListHeader>Radio buttons</Ons.ListHeader>}
+          renderRow={this.renderRadioRow}
+        />
+
+        <Ons.List
+          dataSource={this.state.vegetables}
+          renderHeader={() => <Ons.ListHeader>Checkboxes</Ons.ListHeader>}
+          renderRow={this.renderCheckboxRow}
+        />
+      </Ons.Page>
+    );
+  }
 });
 
-ReactDOM.render(<App />, document.getElementById('react'));
+ons.ready(function() {
+  ReactDOM.render(<MyPage />, document.getElementById('react'));
+});
